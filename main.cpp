78b49b5f84b45718f9cc3ea3da24557b2ec7d280
Vish-web25 <vishwarajjadhav139@gmail.com>
2026-08-06 15:33:31 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *left, *right;
+};
+Node* newNode(int data)
+{
+    Node* node = new Node;
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+void preorder(Node* root)
+{
+    if (root == NULL)
+        return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(Node* root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+void postorder(Node* root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+int main()
+{
+    Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    cout << "Preorder: ";
+    preorder(root);
+    cout << "\nInorder: ";
+    inorder(root);
+    cout << "\nPostorder: ";
+    postorder(root);
+    return 0;
+}
